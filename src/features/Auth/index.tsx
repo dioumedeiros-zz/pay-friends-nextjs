4eb36ae20585c07bdc1ffb8e64c1f128490e75bf
Diou Medeiros <diou.182@gmail.com>
2022-01-18 22:34:24 -0300

    :sparkles: Created form control and navigate

--- src/features/Auth/index.tsx
+++ src/features/Auth/index.tsx
@@ -1,58 +1,72 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import {
   Box,
   Button,
   Flex,
-  FormControl,
   FormLabel,
   Heading,
   Input,
   VStack,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
 
-import Logo from 'assets/Logo.svg';
-import Signinbackground from 'assets/signinbackground.svg';
+import Logo from '~/assets/Logo.svg';
+import Signinbackground from '~/assets/signinbackground.svg';
 
-const Auth = () => (
-  <Box display="flex" flexDirection="row">
-    <Flex h="100vh" w="3xl">
-      <VStack
-        w="full"
-        h="full"
-        p={10}
-        justifyContent="center"
-        alignItems="flex-start"
-        bg="gray.100"
-      >
-        <VStack alignItems="flex-start" spacing={5} mb={10}>
-          <Image src={Logo} alt="Logo" />
-          <Heading size="lg">Bem vindo de volta</Heading>
+const Auth = () => {
+  const route = useRouter();
+  const { register, handleSubmit } = useForm();
+
+  const handleLogin = (data: any) => {
+    console.log('data ', data);
+    route.push('dashboard');
+  };
+
+  return (
+    <Box display="flex" flexDirection="row">
+      <Flex h="100vh" w="3xl">
+        <VStack
+          w="full"
+          h="full"
+          p={10}
+          justifyContent="center"
+          alignItems="flex-start"
+          bg="gray.100"
+        >
+          <VStack alignItems="flex-start" spacing={5} mb={10}>
+            <Image src={Logo} alt="Logo" />
+            <Heading size="lg">Bem vindo de volta</Heading>
+          </VStack>
+
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <FormLabel>E-mail</FormLabel>
+            <Input
+              placeholder="E-mail"
+              mb={5}
+              {...register('email', { required: true })}
+            />
+
+            <FormLabel>Senha</FormLabel>
+            <Input
+              type="password"
+              placeholder="senha"
+              {...register('password')}
+            />
+
+            <Button w="full" mt={8} size="lg" colorScheme="blue" type="submit">
+              ENTRAR
+            </Button>
+          </form>
         </VStack>
-
-        <FormControl>
-          <FormLabel>E-mail</FormLabel>
-          <Input placeholder="E-mail" mb={5} />
-          <FormLabel>Senha</FormLabel>
-          <Input placeholder="senha" />
-          <Button
-            w="full"
-            mt={8}
-            size="lg"
-            colorScheme="blue"
-            type="submit"
-            varian
-          >
-            ENTRAR
-          </Button>
-        </FormControl>
-      </VStack>
-    </Flex>
-    <Flex h="100vh" w="100vw">
-      <VStack w="full" h="full" p={10} justifyContent="center">
-        <Image src={Signinbackground} alt="SignIn image" />
-      </VStack>
-    </Flex>
-  </Box>
-);
+      </Flex>
+      <Flex h="100vh" w="100vw">
+        <VStack w="full" h="full" p={10} justifyContent="center">
+          <Image src={Signinbackground} alt="SignIn image" />
+        </VStack>
+      </Flex>
+    </Box>
+  );
+};
 
 export default Auth;
