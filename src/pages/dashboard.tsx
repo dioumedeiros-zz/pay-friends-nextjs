f7baaa72a6cb4736df0e52a268cd372c6b8fd80c
Diou Medeiros <diou.182@gmail.com>
2022-01-20 23:46:30 -0300

    :sparkles: feat: Created new Dashboard feature

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import { DashboardList } from '~/features/Dashboard';
 
 const Dashboard: NextPage = () => {
-  return <h1>Veio</h1>;
+  return (
+    <>
+      <h1>Veio</h1>
+      <DashboardList />
+    </>
+  );
 };
 
 export default Dashboard;
